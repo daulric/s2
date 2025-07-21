e6c20b01dae91eb9f449de0329ff14208646a38b
Ulric <57965846+daulric@users.noreply.github.com>
2025-07-21 13:49:22 -0400

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,13 +4,16 @@ import { NextRequest } from 'next/server'
 
 export async function createClient(req?: NextRequest) {
   const cookieStore = await cookies()
+  const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabase_anon_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const db_schema = process.env.NEXT_PUBLIC_SCHEMA || "public"
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabase_url,
+    supabase_anon_key,
     {
       db: {
-        schema: process.env.NEXT_PUBLIC_SCHEMA,
+        schema: db_schema,
       },
 
       cookies: {
