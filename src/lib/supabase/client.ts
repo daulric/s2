b1ae0ce16962594a873260c734fbd0f17a14a0b5
Ulric <57965846+daulric@users.noreply.github.com>
2025-07-21 13:50:05 -0400

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,12 +3,16 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
+  const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabase_anon_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const db_schema = process.env.NEXT_PUBLIC_SCHEMA || "public"
+  
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabase_url,
+    supabase_anon_key,
     {
       db: {
-        schema: process.env.NEXT_PUBLIC_SCHEMA,
+        schema: db_schema,
       }
     }
   )
