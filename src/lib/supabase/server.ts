3cae3861ffb8bc1f15455af4caf869297baae0a7
daulric <ulricaird1@gmail.com>
2025-10-16 15:45:53 -0400

    refactor: remove subdomain handling logic from middleware

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -9,11 +9,6 @@ export async function createClient(req?: NextRequest) {
   const supabase_service_role_key = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
   const db_schema = process.env.NEXT_PUBLIC_SCHEMA || "public"
 
-  const domain =
-    process.env.NODE_ENV === "production"
-      ? ".daulric.dev"
-      : ".localhost:3000";
-
   return createServerClient(
     supabase_url,
     supabase_service_role_key || supabase_anon_key || "",
@@ -40,7 +35,6 @@ export async function createClient(req?: NextRequest) {
             cookiesToSet.forEach(({ name, value, options }) => {
               options = {
                 ...options,
-                domain,
                 path: "/",
                 sameSite: "lax",
                 secure: process.env.NODE_ENV === "production",
